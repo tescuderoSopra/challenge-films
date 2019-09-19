355389b193f6d86327fdfc8e6dd2e314e3c465c8
Tania Escudero Fernández <tania@Tanias-MacBook-Pro.local>
2019-09-19 14:42:01 +0200

    service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,40 @@
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request))
+const CACHE_NAME = 'cache-challenge';
+self.addEventListener('install', (event) => {
+    // Perform install steps
+    const urlsToCache = [
+        '/'
+    ];
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              if(!response || response.status !== 200) {
+                return response;
+              }
+              var responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
