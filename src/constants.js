b34e826c8a5505d33714fcd722a09870f8d8a762
Tania Escudero Fernández <tania@Tanias-MacBook-Pro.local>
2019-09-20 12:11:15 +0200

    sandbox variable

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,19 @@
 
-const endpoints = {
+const isDebug = window.mode === 'development';
+
+
+let endpoints = {
     urlSearch: "search",
     urlTV: "tv",
     urlMovie: "movie",
     multi: "multi",
-    APIkey: "96befef4ed5f899937a3ab357c0e2a4f",
 }
 
-const constants = {
+const remote = {
     base: "https://api.themoviedb.org/3",
     urlImage: "https://image.tmdb.org/t/p/w300_and_h450_bestv2",
     lastSearchesNumber: 5,
+    APIkey: "96befef4ed5f899937a3ab357c0e2a4f",
     ...endpoints
 };
 
@@ -19,5 +22,13 @@ const local = {
     APIkey: 'testAPI',
     ...endpoints
 }
-export default constants;
+
+if(isDebug) {
+    endpoints = { ...endpoints, ...local };
+} else {
+    endpoints = { ...endpoints, ...remote };
+}
+console.log(endpoints);
+
+export default endpoints;
 
